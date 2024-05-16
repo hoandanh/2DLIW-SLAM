b6e54a377c77eb33ee007690b6b23a33025344f1
chientv2 <chientv2@rikkeisoft.com>
2024-05-16 16:59:19 +0700

    First commit

---
+++ nodes/merge_bags.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Copyright (C) <2019> Intel Corporation
+# SPDX-License-Identifier: MIT
+# Author: Xuesong Shi
+
+"""
+merge given bag files into a single one
+"""
+import rosbag
+import sys
+
+def main():
+    files = sys.argv[1:]
+    outfile = 'merged.bag'
+    outbag = rosbag.Bag(outfile, 'w')
+
+    for file in files:
+        with rosbag.Bag(file) as inbag:
+            print ("-------------------- Input: %s ------------------------------" % file)
+            print (inbag)
+            for topic,msg,t in inbag.read_messages():
+                outbag.write(topic, msg, t)
+
+    outbag.close()
+    outbag = rosbag.Bag(outfile)
+    print ("------------------- Output: %s ------------------------------" % outfile)
+    print (outbag)
+
+if __name__ == '__main__':
+    main()
